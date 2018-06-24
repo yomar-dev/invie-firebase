17b13f4b3087957d45dadac3e8ddeb98b24ddd17
Yomar Miranda <mirandayomar@gmail.com>
2018-06-24 02:57:31 -0500

    Autenticación con Google.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log(firebase.auth());
+const btnLogin = document.getElementById("btnLogin");
+
+firebase.auth().onAuthStateChanged((user) => {
+	console.log(user);
+})
+
+btnLogin.addEventListener('click', (e) => {
+	e.preventDefault();
+	let provider = new firebase.auth.GoogleAuthProvider();
+	provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
+	firebase.auth().signInWithPopup(provider)
+		.then((datosUsuario) => {
+			console.log(datosUsuario);
+		})
+		.catch((error) => {
+			console.log(`Error: ${error.message}`);
+		})
+	console.log("Boton Ok");
+})
